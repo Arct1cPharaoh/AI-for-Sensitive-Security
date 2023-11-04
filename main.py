f34fa55a71e3c7ede9af1116f013a8801ddb9226
Tre'Vaughn Barboza <trerese3@gmail.com>
2023-11-04 18:23:13 -0400

    starpii:)

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from transformers import pipeline
 
-ner_pipeline = pipeline("ner", model="CyberPeace-Institute/SecureBERT-NER")
+ner_pipeline = pipeline(
+    "token-classification",
+    model="bigcode/starpii"
+)
 
 text = "Microsoft Threat Intelligence analysts assess with high confidence that the malware, which we call KingsPawn, is developed by DEV-0196 and therefore strongly linked to QuaDream. We assess with medium confidence that the mobile malware we associate with DEV-0196 is part of the system publicly discussed as REIGN."
 
 ner_results = ner_pipeline(text)
 
-print(ner_results)
+for entity in ner_results:
+    print(entity)
