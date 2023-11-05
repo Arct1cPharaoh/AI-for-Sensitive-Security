8cb13d69a2ffc85ebcbdd7b687046cd8cd256c42
LittleSilver33 <zjsilverstein@gmail.com>
2023-11-05 02:00:08 -0500

    Change Everything

---
+++ ChromeExtension/content.js
@@ -0,0 +1,134 @@
+function debounce(func, wait) {
+    let timeout;
+  
+    return function executedFunction(...args) {
+      const later = () => {
+        clearTimeout(timeout);
+        func(...args);
+      };
+  
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+    };
+  }
+  
+  function checkForComposeBox() {
+      const composeBox = document.querySelector('div[aria-label="Message Body"]');
+      if (composeBox) {
+          // Detected compose box, now check for sensitive information
+          debouncedDetectSensitiveInformation(composeBox.innerText);
+      }
+  }
+
+    function insertStatusIcon() {
+        const composeBox = document.querySelector('div[aria-label="Message Body"]');
+        if (composeBox) {
+            let statusIcon = document.querySelector('#status-icon');
+            if (!statusIcon) {
+                // Status Icon
+                statusIcon = document.createElement('img');
+                statusIcon.id = 'status-icon';
+                statusIcon.style.position = 'absolute';
+                statusIcon.style.bottom = '10px';
+                statusIcon.style.right = '10px';
+                statusIcon.style.width = '20px';
+                statusIcon.style.height = '20px';
+                statusIcon.src = chrome.runtime.getURL('img/safe.png'); // Default icon
+                composeBox.parentElement.style.position = 'relative';
+                composeBox.parentElement.appendChild(statusIcon);
+
+                // Loading Icon
+                loadingIcon = document.createElement('img');
+                loadingIcon.id = 'loading-icon';
+                loadingIcon.style.position = 'absolute';
+                loadingIcon.style.bottom = '10px';
+                loadingIcon.style.right = '10px';
+                loadingIcon.style.width = '20px';
+                loadingIcon.style.height = '20px';
+                loadingIcon.src = chrome.runtime.getURL('img/loading.gif');
+                loadingIcon.style.display = 'none'; // Initially hidden
+                composeBox.parentElement.appendChild(loadingIcon);
+            }
+        }
+    }
+
+    function showLoadingIcon() {
+        const loadingIcon = document.querySelector('#loading-icon');
+        if (loadingIcon) {
+            loadingIcon.style.display = 'block'; // Show loading icon
+        }
+    }
+
+    function hideLoadingIcon() {
+        const loadingIcon = document.querySelector('#loading-icon');
+        if (loadingIcon) {
+            loadingIcon.style.display = 'none'; // Hide loading icon
+        }
+    }
+
+    function updateStatusIcon(isSensitive) {
+        const statusIcon = document.querySelector('#status-icon');
+        if (statusIcon) {
+            statusIcon.src = isSensitive ? chrome.runtime.getURL('img/unsafe.png') : chrome.runtime.getURL('img/safe.png');
+        }
+    }
+  
+  function detectSensitiveInformation(text) {
+      showLoadingIcon();
+  
+      // INSERT LOGIC HERE
+      let isSensitive = text.includes("qwerty"); // Temporary (For Testing Purposes Only)
+  
+      // Updates Icon to stop loading and indicate safety of text
+      updateStatusIcon(isSensitive);
+      hideLoadingIcon();
+  
+      if (isSensitive) {
+          chrome.runtime.sendMessage({ status: 'unsafe' });
+      } else {
+          chrome.runtime.sendMessage({ status: 'safe' });
+      }
+  
+      fetch('http://localhost:5000/classify', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ text: text })
+      })
+      .then(response => response.json())
+      .then(data => {
+          console.log('Entities:', data);
+      })
+      .catch((error) => {
+          console.error('Error:', error);
+      });
+  }
+  
+  // Debounce the detectSensitiveInformation function
+    debouncedDetectSensitiveInformation = debounce(detectSensitiveInformation, 2000);
+  
+  if (!window.hasObserver) {
+      const observer = new MutationObserver((mutations) => {
+          mutations.forEach((mutation) => {
+              if (mutation.addedNodes.length || mutation.removedNodes.length) {
+                  checkForComposeBox();
+              }
+          });
+      });
+  
+      observer.observe(document.body, { childList: true, subtree: true });
+      window.hasObserver = true;
+  }
+  
+  // Listen for changes in the compose box with debounced function
+  document.addEventListener('input', (event) => {
+      if (event.target.matches('div[aria-label="Message Body"]')) {
+          debouncedDetectSensitiveInformation(event.target.innerText);
+      }
+  });
+  
+  // Initial check when the script is injected
+  checkForComposeBox();
+  insertStatusIcon();
+  
--- app.py
+++ app.py
@@ -1,27 +1,30 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS, cross_origin
-import idenifier
+from flask_cors import CORS
+import identifier
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/classify": {"origins": "https://mail.google.com"}})
 
 @app.route('/classify', methods=['POST'])
-@cross_origin()
 def classify_text():
-    print("HI")
-    data = request.json
-    text = data['text']
+    app.logger.info("Classification request received")
+    if not request.json or 'text' not in request.json:
+        app.logger.error("Invalid request: No JSON or 'text' field missing")
+        return jsonify({"error": "Bad request"}), 400
 
-    # TODO: Mightnot need this
+    text = request.json['text']
     max_length = 512
     truncated_text = text[:max_length]
+    app.logger.info(f"Truncated text: {truncated_text}")
 
-    print(truncated_text)
-
-    entities = idenifier.identify_sensitive_entities(truncated_text)
-    for entity_type, entity_text in entities:
-        print(f"{entity_type} - {entity_text}")
-    return jsonify(entities)
+    try:
+        entities = identifier.identify_sensitive_entities(truncated_text)
+        for entity_type, entity_text in entities:
+            app.logger.info(f"{entity_type} - {entity_text}")
+        return jsonify(entities)
+    except Exception as e:
+        app.logger.error(f"An error occurred: {e}")
+        return jsonify({"error": "Internal server error"}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True)  # Set debug=False for production
